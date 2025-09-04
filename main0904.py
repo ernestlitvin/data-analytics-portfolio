eebeb9e4c3871d62df9465d3d0ffe49f9495dead
Ernest Litvin <erik.litvin@gmail.com>
2025-09-04 12:17:20 +0300

    Continue with practising 12:17

--- main0904.py
+++ main0904.py
@@ -24,11 +24,88 @@
 import random
 
 print()
-# Sukurkite tris kintamuosius ir naudodamiesi funkcija random.randint(x,x) jiems priskirkite atsitiktines reikšmes nuo 0 iki 25.
-# Raskite ir atspausdinkite kintąmąjį turintį vidurinę reikšmę.
 
-kin1 = random.randint(0,25)
-kin2 = random.randint(0,25)
-kin3 = random.randint(0,25)
+item_name = "Banana"
+quantity = 5
+price = 7.45
 
-print(kin1,kin2,kin3)
+total_price = quantity * price
+
+print("Receipt: ")
+print("Item: ", item_name)
+print("Quantity: ", quantity)
+print("Total price: ", total_price)
+print()
+
+# name = input("What is your name? ")
+# color = input("What is your favourite color")
+# print(f"Hello {name}! Your favourite color is {color}")
+# print()
+#
+# a = input("Please enter the width of the square ")
+# a_int = int(a)
+# b = input("Please enter the height of the square ")
+# b_int = int(b)
+# a_b = a_int*b_int
+# print(f"The area of square is {a_b}  ")
+
+# subjects = ["math", "history", "pe", "geography", "english language"]
+# second_item = subjects[1]
+# print(second_item)
+# subjects.append("lithuanian language")
+# subjects.insert(0,"german language")
+# print(subjects)
+# print(len(subjects))
+
+# num = random.randint(-5,5)
+# print(num)
+# if num > 0:
+#     print("The number is positive")
+# elif num < 0:
+#     print("The number is negative")
+# else:
+#     print("The number is zero")
+#
+# is_logged_in = True
+# is_admin = True
+# if is_logged_in and is_admin:
+#     print("Hello admin")
+# else:
+#     print("Hello user")
+#
+# psw = input("What is the password ?")
+# # psw = random.randint(1, 3)
+# # print(psw)
+# if psw == "12345":
+#     print("Password is correct")
+# else:
+#     print("Password is incorrect")
+
+# rating = int(input("Please tell your rating: "))
+# mot = input ("Do you have motivation letter ?")
+# orating = int(input ("What is your outside rating ?"))
+#
+# if (rating >= 85) and (mot == "Yes" or orating >= 7):
+#         print("Accepted")
+# else:
+#     print("Rejected")
+
+### alternative way
+# rating = int(input("Please tell your rating: "))
+# mot_answer = input("Do you have motivation letter? (yes/no) ")
+# orating = int(input("What is your outside rating? "))
+#
+    # has_motivation_letter = False
+    # if mot_answer.lower() == "yes":
+    #     has_motivation_letter = True
+    #
+    # or even: if mot_answer.lower() == "yes":
+#     has_motivation_letter = True
+# else:
+#     has_motivation_letter = False
+
+# if rating >= 85 and (has_motivation_letter or orating > 7):
+#     print("Accepted")
+# else:
+#     print("Rejected")
+
