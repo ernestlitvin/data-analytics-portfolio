38d92886dbf4c9172fe4942f31ad01e8d9b5e4cc
Ernest Litvin <erik.litvin@gmail.com>
2025-09-09 17:36:28 +0300

    Continue with practising 17:36

--- main0909.py
+++ main0909.py
@@ -91,34 +91,75 @@ newstarWars = starWars.split() # the best
 print(newstarWars)
 print(newstarWars[3])
 
+print()
+print("-----")
 # Suskaičiuoti kiek stringe "Don't Be a Menace to South Central While Drinking Your Juice in the Hood"
 # yra žodžių trumpesnių arba lygių nei 5 raidės.
 # Pakartokite kodą su stringu "Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale"
 
 sent = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood"
+newsent = sent.split()
+print(sent)
+print(newsent)
+length = len(newsent)
+print(length)
+print(newsent[0])
 
+count_words = 0
 
+if len(newsent[0]) <= 5:
+    count_words += 1
+if len(newsent[1]) <= 5:
+    count_words += 1
+if len(newsent[2]) <= 5:
+    count_words += 1
+if len(newsent[3]) <= 5:
+    count_words += 1
+if len(newsent[4]) <= 5:
+    count_words += 1
+if len(newsent[5]) <= 5:
+    count_words += 1
+if len(newsent[6]) <= 5:
+    count_words += 1
+if len(newsent[7]) <= 5:
+    count_words += 1
+if len(newsent[8]) <= 5:
+    count_words += 1
+if len(newsent[9]) <= 5:
+    count_words += 1
+if len(newsent[10]) <= 5:
+    count_words += 1
+if len(newsent[11]) <= 5:
+    count_words += 1
+if len(newsent[12]) <= 5:
+    count_words += 1
+if len(newsent[13]) <= 5:
+    count_words += 1
 
+print(f"Count of <=5 words - {count_words}")
+print()
+print("----")
 
+## --> alternative way
+sent = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood"
+newsent = sent.split()
+print(newsent)
+print(len(newsent))
+short = 0
+for letters in newsent:
+    if len(letters) <= 5:
+        short += 1
+print(short)
 
-# sent = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood"
-#newsent = sent.split() ## --> alternative way
-#print(newsent)
-# print(len(newsent))
-# short = 0
-# for letters in newsent:
-#     if len(letters) <= 5:
-#         short += 1
-# print(short)
-#
-# print("-----")
-# sent2 = "Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale"
-# newsent2 = sent2.split()
-# print(newsent2)
-# print(len(newsent2))
-# short2 = 0
-#
-# for letters in newsent2:
-#     if len(letters) <= 5:
-#         short2 += 1
-# print(short2)
+print("-----")
+sent2 = "Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale"
+newsent2 = sent2.split()
+print(newsent2)
+print(len(newsent2))
+short2 = 0
+
+for letters in newsent2:
+    if len(letters) <= 5:
+        short2 += 1
+print(short2)
+
