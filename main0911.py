e0603a80294cac40d9f0a206af46e936d292db2f
Ernest Litvin <erik.litvin@gmail.com>
2025-09-11 13:23:38 +0300

    Continue with practising - loops -  13:23

--- main0911.py
+++ main0911.py
@@ -66,13 +66,22 @@ for row in range(5):
         print("* ", end=" ")
     print()
 
-
-
-
-
 # Prieš tai nupieštam kvadratui nupieškite istrižaines zaigzdutę pakeisdami kitu simboliu.
+print("-------")
 
+# (0,0) (0,1) (0,2) (0,3) (0,4)
+# (1,0) (1,1) (1,2) (1,3) (1,4)
+# (2,0) (2,1) (2,2) (2,3) (2,4)
+# (3,0) (3,1) (3,2) (3,3) (3,4)
+# (4,0) (4,1) (4,2) (4,3) (4,4)
 
+for x in range(5):
+    for y in range(5):
+        if x == y or x + y == 4:
+            print("# ", end=" ")
+        else:
+            print("* ", end=" ")
+    print()
 
 
 
