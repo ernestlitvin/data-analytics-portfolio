4da781876fef43914576e70a2d19bb23a8874454
Ernest Litvin <erik.litvin@gmail.com>
2025-09-11 10:44:52 +0300

    Continue with practising - loops -  10:44

--- main0911.py
+++ main0911.py
@@ -1,5 +1,6 @@
 # Sugeneruokite 300 atsitiktinių skaičių nuo 0 iki 300, atspausdinkite juos atskirtus tarpais ir suskaičiuokite
 # kiek tarp jų yra didesnių už 150.  Skaičiai didesni nei 275 turi būti atspausdinti skliausteliuose” [ ] “.
+import random
 
 print()
 print("-----")
@@ -17,5 +18,49 @@ for _ in range(300):
 print()
 print(f"Numbers, which are bigger than 150 - {nr_count_150}")
 
+## alternative, better way
+
+# skaiciai = []
+# count = 0
+
+# for _ in range(300):
+#     skaicius = random.randint(1,300)
+#     skaiciai.append(skaicius)
+#     if skaicius > 150:
+#         count += 1
+#
+# for skaicius in skaiciai:
+#     if skaicius > 275:
+#         print(f"[{skaicius}]", end=" ")
+#     else:
+#         print(skaicius, end=" ")
+# print()
+# print("Number of values greater than 150:", count)
+
 print()
 print("------")
+
+# Vienoje eilutėje atspausdinkite visus skaičius nuo 1 iki 3000, kurie dalijasi iš 77 be liekanos.
+# Skaičius atskirkite kableliais. Po paskutinio skaičiaus kablelio neturi būti.
+
+nums = []
+#
+for nr in range(1,3001):
+    if nr % 77 == 0:
+        nums.append(nr)
+
+print(nums)
+
+print("-------")
+
+results = []
+for numbers in range(1,3001):
+    if numbers % 77 == 0:
+        results.append(numbers)
+print(", ".join(str(x) for x in results))
+
+
+
+
+
+
