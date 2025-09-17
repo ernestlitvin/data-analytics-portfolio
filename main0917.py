cfa8a52f226647246ee0b4d184becccc9c5ebee4
Ernest Litvin <erik.litvin@gmail.com>
2025-09-17 09:40:13 +0300

    Continue with practising - functions

--- main0917.py
+++ main0917.py
@@ -3,9 +3,9 @@
 mass = [1, "hello", 2.5, True, 10]
 
 def numbers_checker(mass):
-    for num in mass:
-        if num == isinstance(num, int):
-            print(num)
+    for item in mass:
+        if isinstance(item, (int, float)):
+            print(item)
 
 numbers_checker(mass)
 
