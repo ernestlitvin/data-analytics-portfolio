43c890f53e573ca4082dc80fd7941be661e3e1ac
Ernest Litvin <erik.litvin@gmail.com>
2025-09-17 13:09:14 +0300

    Continue with practising - functions

--- main0917.py
+++ main0917.py
@@ -1,10 +1,29 @@
 # Sukurkite funkciją, kuri priimtų masyvą ir atspausdintų tik tuos elementus kurie yra skaičiai.
+# mass = [1, "hello", 2.5, True, 10, -20, -55.5]
+# def numbers_checker(mass):
+#     for item in mass:
+#         if isinstance(item, (int, float)) and not isinstance(item, bool):
+#             print(item)
+# numbers_checker(mass)
 
-mass = [1, "hello", 2.5, True, 10]
+# Sukurkite funkciją, kuri priima masyvą ir atspausdina tik sveikuosius skaičius.
+# (jei pavyks, patobulinkite, kad funkcija priimtų antrą parametrą True/False kuris nuspręstų ar spausdins tik sveikuosius skaičius
+# ar skaičius su kableliu.
+print()
+print("-----")
 
-def numbers_checker(mass):
-    for item in mass:
-        if isinstance(item, (int, float)) and not isinstance(item, bool):
-            print(item)
-numbers_checker(mass)
+def whole_number(massive, print_only_integers=True):
+    print(f"--- Print only whole = {print_only_integers} ---")
+    for item in massive:
+        if print_only_integers == True:
+            if isinstance(item, int) and not isinstance(item, bool) :
+                print(item, end=" ")
 
+        else:
+            if isinstance(item, (int, float)) and not isinstance(item, bool):
+                print(item, end=" ")
+    print("\n")
+
+massive = [-534.3, -200, -12.4, 0, 1, 5, 21, 543, 23.5, 453.3, "dog", "no", True, False ]
+whole_number(massive, True)
+whole_number(massive, False)
