dbabd45b0a91d4c8c2818f3075a024f6c2aa7ca6
Ernest Litvin <erik.litvin@gmail.com>
2025-09-22 15:27:45 +0300

    Continue with practising - Pandas

--- main0922.py
+++ main0922.py
@@ -19,5 +19,35 @@ print(f"Collums names: {employees_df.columns}")
 print("Type of data in collums:")
 print(employees_df.dtypes)
 
+print("----")
+print()
+
+solar = pd.Series(
+    data = [4879, 12104, 12742, 6779, 139822],
+    index = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"]
+)
+print(solar)
+print("----")
+print()
+
+games = {
+"Name" : ["The Witcher 3: Wild Hunt", "Counter-Strike: Global Offensiv", "Minecraft", "Cyberpunk 2077"],
+"Year" : [2015, 2012, 2011, 2020],
+"Genre" : ["RPG", "FPS", "Survival", "Action RPG"]
+}
+games_df = pd.DataFrame(games)
+print(games_df)
+print("----")
+print(f"Size of table {games_df.shape}")
+print("----")
+print(f"Collums names: {games_df.columns}")
+print("----")
+print("Type of data in collums: ")
+print(games_df.dtypes)
+
+
+
+
+
 
 
