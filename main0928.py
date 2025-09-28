829417c8c4b098ffa86ae12f88ecb4c4a1315d6c
Ernest Litvin <erik.litvin@gmail.com>
2025-09-28 12:52:49 +0300

    Continue with practising Pandas

--- main0928.py
+++ main0928.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+## pd.concat() — simple "glue" pages (vertically)
+
 # Employee table
 staff_df = pd.DataFrame({
     'employee_id': [101, 102, 103, 104],
@@ -15,3 +17,26 @@ print("--- Employees ---")
 print(staff_df)
 print("\n--- Salary ---")
 print(salary_df)
+
+# example: the new record of the new employee
+new_staff_df = pd.DataFrame({
+    'employee_id': [106],
+    'name': ['Sergio'],
+    'department': ['IT']
+})
+# "Glue" old and new table (vertical)
+all_staff = pd.concat([staff_df, new_staff_df])
+print(all_staff)
+
+## pd.merge() — SQL's JOIN analog
+## Syntax : pd.merge(left_table, right_table, on='key_table', how='тип_объединения')
+# how (join's type') — most important parametr:
+# inner (internal) (default): Leaves only those rows for which there is a key in both tables.
+# left (left): Take all the rows from the left table. If any of them do not match to the right, there will be blanks (NaN).
+# right (right): Reverse, all rows are taken from the right table.
+# outer (full): Takes all rows from both tables.
+
+
+
+
+
