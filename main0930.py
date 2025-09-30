11f4d55481db3917da47304d039a965be539e0a8
Ernest Litvin <erik.litvin@gmail.com>
2025-09-30 11:22:35 +0300

    Continue with practising Seaborn+Pandas+Matplotlib

--- main0930.py
+++ main0930.py
@@ -18,6 +18,25 @@ full_df = pd.read_csv("full_df.csv")
 # # This is value_counts() graphically.
 # plt.show()
 
+# sns.barplot(data=full_df, x="category", y="Revenue")
+# plt.title("Avg revenue per category") # Add a header using Matplotlib
+# plt.show()
+# Shows the average (default) value of the numeric variable for each category.
+# Ideal for answering the question: "In which group is the average higher?".
+
+# sns.boxplot(data=full_df, x="category", y="Revenue")
+# plt.title("Distribution of revenues by categories")
+# plt.show()
+## boxplot - "Box with a mustache" 📦
+# Purpose: This is a more powerful tool than barplot. It shows the full distribution of the numerical variable within each category.
+# Helps answer the questions: "What is the median? How big is the spread? Are there emissions?".
+# How to read "box":
+# The line inside the box is the median (50% less values, 50% more).
+# Box boundaries are the 25th and 75th percentiles (data center).
+# "Moustache" - shows the main spread of data.
+# Points outside the mustache are emissions (abnormally high or low values).
+
+print("--tasks--")
 # sns.scatterplot(data=full_df, x="price_per_item", y="quantity")
 # plt.show() # clients are buying cheapest products more often, than expensive
 # sns.histplot(data=full_df, x="quantity")
